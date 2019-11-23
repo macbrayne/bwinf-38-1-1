c4fdc47ce8b7fa9687d01a85f0283dec52159abd
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2019-11-23 22:59:46 +0100

    Improved Flower javadoc

--- src/main/java/Flower.java
+++ src/main/java/Flower.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 import java.util.Random;
 
+/**
+ *
+ */
 class Flower {
     Colors color;
 
@@ -12,12 +15,21 @@ class Flower {
         return new Flower(Colors.fromValue(new Random().nextInt(Colors.size()) + 1));
     }
 
+    /**
+     * Flower Colors
+     */
     enum Colors {
         BLUE("blau", 1), YELLOW("gelb", 2),
         GREEN("gruen", 3), ORANGE("orange", 4),
         PINK("rosa", 5), RED("rot", 6),
         TURQUOISE("tuerkis", 7);
+        /**
+         * German version of the name
+         */
         final private String parser;
+        /**
+         * int representation in the program
+         */
         final private int value;
 
         Colors(String parser, int value) {
@@ -32,6 +44,10 @@ class Flower {
             return null;
         }
 
+        /**
+         * @param value Int value
+         * @return Returns the Color corresponding to a integer
+         */
         static Colors fromValue(int value) {
             return Arrays.stream(Colors.values()).filter(i -> i.value == value).findAny().orElseThrow(() -> new IllegalStateException(String.format("Unsupported type %s", value)));
         }
